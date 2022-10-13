35e15bfb3c4917a027cdb4255eef40f1344f3aa3
karengcecena <karengcecena@gmail.com>
2022-10-12 17:01:15 -0700

    Lab First Part Complete

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -13,17 +13,22 @@ function showFortune(evt) {
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
+
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
   evt.preventDefault();
 
-  // const url = '/weather.json';
+  const url = '/weather.json'
   const zipcode = document.querySelector('#zipcode-field').value;
-  const queryString = new URLSearchParams({ "zipcode": `${zipcode}` }).toString();
-  const url = `/weather.json?${queryString}`;
 
+  // setting url version 1 (complex)
+  // const queryString = new URLSearchParams({ "zipcode": `${zipcode}` }).toString();
+  // const url = `/weather.json?${queryString}`;
+
+  // setting url version 2 (simpler)
   // const url = `/weather.json?zipcode=${zipcode}`;
+
   fetch(url)
     .then((response) => response.json())
     .then((weather_info) => {
@@ -33,12 +38,45 @@ function showWeather(evt) {
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
+
 // PART 3: ORDER MELONS
+
+
+
+
+
+/////////////////////////
 
 function orderMelons(evt) {
   evt.preventDefault();
 
   // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
-document.querySelector('#order-form').addEventListener('submit', orderMelons);
+  const url = "/order-melons.json"
+  const formInputs = {
+    melon_type: document.querySelector("#melon-type-field").value,
+    qty: document.querySelector("#qty-field").value,
+  };
+
+  fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(formInputs),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+  })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+      if (responseJson.code === "OK"){
+        document.querySelector('#order-status').innerHTML = responseJson.msg;
+        document.querySelector('#order-status').classList.remove("order-error")
+      } else{
+        document.querySelector('#order-status').innerHTML = responseJson.code;
+        // document.querySelector('#order-status').innerHTML = responseJson.msg;
+        document.querySelector('#order-status').classList.add("order-error")
+      }
+      console.log(responseJson);
+    });
+  };
+
+  document.querySelector('#order-form').addEventListener('submit', orderMelons);
